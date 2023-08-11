c777a553f2033bf024690cc8a86b88ba2d8be9eb
iDominate <ahmedpython2018@.com>
2023-08-11 06:05:57 -0700

    kj

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,16 +2,16 @@
 
 
 /**
- *
  * main - Entry point
  *
- * returns: Always 0(Success)
+ * Return: Always 0(Success)
  */
 int main(void)
 {
 	char alpha;
- 	for(alpha='z'; alpha >= 'a'; alpha--)
- 	{
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
 		putchar(alpha);
 	}
 	putchar('\n');
