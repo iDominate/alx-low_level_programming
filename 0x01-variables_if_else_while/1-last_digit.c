0e1385931ff9bbce9c0fa6559761f5af557b1f46
iDominate <ahmedpython2018@.com>
2023-08-11 04:34:28 -0700

    kj

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	/* your code goes there */
-	if((-last_digit) > 5)
+	if( (-last_digit) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
 	}
-	else if((-last_digit) ==  0)
+	else if( (-last_digit) ==  0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}else
