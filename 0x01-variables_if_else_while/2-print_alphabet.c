d7c8f19b3eed01224035fed164b282c76da5e233
iDominate <ahmedpython2018@.com>
2023-08-11 05:43:20 -0700

    kj

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,12 @@
 
 
 
-/**
- * main - Entry point
- *
- * Return: Always 1 (Error)
- */
-
-
 int main(void)
 {
-	char i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		putchar(i);
+	char alpha;
+ 	for(alpha='a'; alpha <= 'z'; alpha++)
+ 	{
+		putchar(alpha);
 	}
 	putchar('\n');
 	return (0);
