c5fb2ca5fa453ed367dd392e53a113988a2c76db
iDominate <ahmedpython2018@.com>
2023-08-11 06:09:16 -0700

    kj

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
 	char hexa;
-	for(hexa=48; hexa <= 57;hexa++)
+
+	for (hexa = 48; hexa <= 57; hexa++)
 	{
 		putchar(hexa);
 
 	}
-	for(hexa=97; hexa <= 102; hexa++)
+	for (hexa = 97; hexa <= 102; hexa++)
 	{
 		putchar(hexa);
 	}
