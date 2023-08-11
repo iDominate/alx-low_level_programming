ba0784a3b79461eb0bbef1cc06520edcd795dc80
iDominate <ahmedpython2018@.com>
2023-08-11 06:13:59 -0700

    kj

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
 	char number;
-	for(number=48; number <= 57;number++)
+
+	for (number = 48; number <= 57; number++)
 	{
 		putchar(number);
-		
-		if(number != 57)
+		if (number != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	
 	putchar('\n');
 	return (0);
 
