7da772d63c023f4d6788d0c06a2fd066e4f77c8b
iDominate <ahmedpython2018@.com>
2023-08-15 04:32:39 -0700

    ytry

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,11 @@
 /**
  * _islower - checks whether the given ascii is lower
  *
+ * ch: the given ascii number
+ *
  * Return: lower 1(True) | upper 0(False)
  */
 int _islower(int ch)
 {
-	return islower(ch) ? 1 : 0;
+	return (islower(ch) ? 1 : 0);
 }
