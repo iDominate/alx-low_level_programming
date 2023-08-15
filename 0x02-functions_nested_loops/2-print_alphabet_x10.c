d2b780e311c3f1119671b5607d446331dbb74e98
iDominate <ahmedpython2018@.com>
2023-08-15 04:19:24 -0700

    ytry

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+
+/**
+ * print_alphabet_10X - prints the alphabet 10 times
+ *
+ * Return: Always void
+ */
+void print_alphabet_x10(void)
+{
+	int times;
+	char ch;
+	for (times = 0; times <= 9; times++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 #include "1-alphabet.h"
 #include "_putchar.h"
+#include "2-print_alphabet_x10.h"
