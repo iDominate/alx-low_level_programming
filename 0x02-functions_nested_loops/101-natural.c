6ac0cf9e43c5bf48f5dd0177e7b57d342f7279fc
iDominate <ahmedpython2018@.com>
2023-08-15 10:48:35 -0700

    ht

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i, result;
+
+	result = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			result += i;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+
+}
