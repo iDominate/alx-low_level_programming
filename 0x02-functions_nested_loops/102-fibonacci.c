2395726e7325752154b6d6c42c863ab640772b85
iDominate <ahmedpython2018@.com>
2023-08-15 12:35:29 -0700

    jy

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Return: fibonacci numbers
+ */
+int main(void)
+{
+	int number_counter;
+	long int iter, previous_number, med;
+
+	previous_number = number_counter =  0;
+	iter = 1;
+	while (number_counter <= 50)
+	{
+		med = iter;
+		iter = previous_number + iter;
+		previous_number = med;
+
+		printf("%ld", iter);
+		if (number_counter != 50)
+			printf(", ");
+		number_counter++;
+	}
+	printf("\n");
+	return (0);
+}
