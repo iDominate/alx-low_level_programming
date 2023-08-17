f27c22df93cfb4968af8dd4d6cfb81576b888264
iDominate <ahmedpython2018@.com>
2023-08-17 02:13:25 -0700

    more functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,14 +11,13 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n < 0)
-		return;
 	for (j = 0; j < n; j++)
 	{
 		for (i = 0; i < j; i++)
 		{
 			_putchar(' ');
 		}
+		if (!(n <= 0))
 		_putchar('\\');
 		_putchar('\n');
 	}
