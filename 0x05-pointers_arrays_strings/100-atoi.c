fa54fdd40c7a5ad4269b8102c5f44313a01eb56f
iDominate <ahmedpython2018@gmail.com>
2023-08-21 20:35:56 +0300

    hhy

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,29 +11,17 @@
  */
 int _atoi(char *s)
 {
-	char buffer[10];
-	int index;
-	size_t i;
+	unsigned int n = 0;
+	int sign = 1;
 
-	index = 0;
-	buffer[0] = '\0';
-	if (atoi(s) != 0 && s[0] != '0')
-		return (atoi(s));
-	else if (s[0] == '0')
-	{
-		return (0);
-	}
+	do {
+		if (*s == '-')
+			sign *= -1
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0')
+		else if (n > 0)
+			break
+	} while (*s++)
 
-		for (i = 0; i < strlen(s); i++)
-		{
-			if (isalpha(s[i]) && strlen(buffer) != 0)
-				break;
-			if (isdigit(s[i]) || (s[i] == '-' && isdigit(s[i + 1])))
-			{
-				buffer[index] = s[i];
-				index++;
-			}
-		}
-
-	return (atoi(buffer));
+	return (n * sign);
 }
