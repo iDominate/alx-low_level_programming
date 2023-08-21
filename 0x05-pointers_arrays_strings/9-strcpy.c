d04640aa28bf4fbfbda2e4e0d8dffd7c4e08c44c
iDominate <ahmedpython2018@gmail.com>
2023-08-21 15:09:30 +0300

    gfh

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+
+/**
+ * _strcpy - copies one string to destination buffer
+ *
+ * @dst: destination
+ * @src: source
+ *
+ * Return: char *
+ */
+char *_strcpy(char *dst, char *src)
+{
+	strcpy(dst, src);
+	return (dst);
+}
