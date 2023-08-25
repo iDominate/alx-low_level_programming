56b120eea82e43e5363171c043301be35fecfe0b
iDominate <ahmedpython2018@gmail.com>
2023-08-25 08:40:54 +0300

    more

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - does the same functionality as strcat()
+ *
+ * @dest: destination buffer
+ * @src: source buffer
+ * @n: the number of bytes
+ * Return: char *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, i1;
+
+	i = 0;
+
+	while (dest[i])
+		i++;
+	for (i1 = 0; ii <= n; i1++)
+	{
+		dest[i++] = src[i1];
+	}
+
+	return (dest);
+}
