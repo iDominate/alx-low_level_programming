ba8d4a44741716f07a79d5f4f435fcd426e9ca69
iDominate <ahmedpython2018@gmail.com>
2023-08-30 12:21:31 +0300

    lki

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _put_recursion - prints a char to stdout
+ * _puts_recursion - prints a char to stdout
  *
  * @s: the string
  *
@@ -9,13 +9,15 @@
  */
 void _puts_recursion(char *s)
 {
-        if(!(*s)){
-                _putchar('\n');
-        }
-        else{
-                _putchar(*s++);
-                _puts_recursion(s);
-        }
+	if (!(*s))
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s++);
+		_puts_recursion(s);
+	}
 }
 
 
