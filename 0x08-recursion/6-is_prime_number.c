b674dd328b7ce68f0ea232ea2db4f3bf13e30043
iDominate <ahmedpython2018@gmail.com>
2023-08-30 17:54:34 +0300

    kjuj

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - is prime number
+ * @n: number
+ *
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	return (is_prime(n, 2));
+}
+
+/**
+ * is_prime - helper function
+ * @x: number
+ * @y: number
+ *
+ * Return: int
+ */
+int is_prime(int x, int y)
+{
+	if (y == 10)
+		return (1);
+	if (x % y == 0 && (x != y))
+		return (0);
+	else
+		return (is_prime(x, y + 1));
+}
+
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-int power_by(int, int);
+int is_prime(int, int);
+int is_prime_number(int);
 
-int _strlen_recursion(int n)
+int is_prime_number(int n)
 {
-		return power_by(n, 2);
+	return is_prime(n, 2);
 }
 
-int power_by(int x, int y)
+int is_prime(int x, int y)
 {
-	if(x % 2 != 0)
-		return -1;
-	if(y * y == x)
-		return y;
-	else
-		return power_by(x, y * 2);
+	if(y == 10)
+		return (1);
+	if (x % y == 0 && (x != y))
+		return (0);
+	else 
+		return is_prime(x, y + 1);
 }
 
 int main(void)
 {
-	printf("%d",_strlen_recursion(1024));
+	printf("%d",is_prime_number(25));
 }
