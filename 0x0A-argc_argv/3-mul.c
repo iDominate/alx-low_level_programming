be661f9e919a288dcc22ee9f15fa56bc564aa485
iDominate <ahmedpython2018@gmail.com>
2023-09-01 14:07:11 +0300

    eyr

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: count
+ * @argv: arg vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	result = 0;
+	if (argc != 3)
+	{
+		puts("Error\n");
+		return (1);
+	}
+	result = _atoi(argv[1]) + _atoi(argv[2]);
+
+	printf("%d", result);
+	return (0);
+}
+
+/**
+ * _atoi - atoi
+ *
+ * @s: string
+ *
+ * Return: number
+ */
+int _atoi(char *s)
+{
+	int a, len, i, j, begin, sign, result = 0;
+
+	len = a = 0;
+	i = 0;
+	begin = j = 1;
+	sign = 1;
+	result = 0;
+	if (*s == '-')
+	{
+		sign *= -1;
+		i = 1;
+		s++;
+	}
+	for (a = 0; s[a]; a++)
+		len++;
+	begin = pow_10(len - 1);
+	for (a = 0; s[a]; a++)
+	{
+		result += (s[a] - 48) * begin;
+		begin /= 10;
+	}
+	return (sign * result);
+}
+
+/**
+ * pow_10 - power by ten
+ *
+ * @n: exponent
+ *
+ * Return: number
+ */
+int pow_10(int n)
+{
+	int i, result;
+
+	result = 1;
+	for (i = 0; i < n; i++)
+	{
+		result *= 10;
+	}
+	return (result);
+}
+
