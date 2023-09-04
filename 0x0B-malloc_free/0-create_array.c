f815f9b7cd47b2c6dd3e36efe146422dc798b488
iDominate <ahmedpython2018@gmail.com>
2023-09-04 10:05:41 +0300

    gfg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,14 @@ char *create_array(unsigned int size, char c)
 {
 	char *p;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	p = (char *) malloc(size * sizeof(char));
 
-	if(!p)
+	if (!p)
 	{
 		return (NULL);
 	}
