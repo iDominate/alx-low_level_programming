7c60020f5c8cc5f7fe7eba195264b24faa8d9e26
iDominate <ahmedpython2018@gmail.com>
2023-09-04 10:01:39 +0300

    gfg

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array and allocates it with c
+ *
+ * @size: size of array
+ * @c: the character to use
+ *
+ * Return: a pointer to the newly created array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = (char *) malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+
+	return (p);
+}
