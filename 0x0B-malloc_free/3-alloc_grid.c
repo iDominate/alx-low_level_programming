87263b86a2fccca88727d5e163ce53606ccba856
iDominate <ahmedpython2018@gmail.com>
2023-09-04 12:22:04 +0300

    gfg

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+
+/**
+ * alloc_grid - allocates memory for a h * w matrix
+ *
+ * @width: w
+ * @height: h
+ *
+ * Return: a pointer to the matrix
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int **n;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	n = calloc(height, sizeof(int **));
+	for (i = 0; i != height; i++)
+	{
+		n[i] = calloc(width, sizeof(int *));
+	}
+	return (n);
+
+}
