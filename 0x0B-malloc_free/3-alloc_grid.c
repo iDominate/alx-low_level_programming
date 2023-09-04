ec8fe14c49e0b9f3af814d832b101d4686c918d3
iDominate <ahmedpython2018@gmail.com>
2023-09-04 13:03:30 +0300

    gfg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,22 +13,24 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **n;
-	
+
 	i = j = 0;
 	n = NULL;
 	if (width == 0 || height == 0)
 		return (NULL);
 	n = (int **) malloc(height * sizeof(int *));
-	if (!n){
+	if (!n)
+	{
 		free(n);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
 		n[i] = (int *) malloc(width * sizeof(int));
-		if (!n[i]){
+		if (!n[i])
+		{
 			free(n);
-			for (j = 0; j <= i;j++)
+			for (j = 0; j <= i; j++)
 				free(n[j]);
 			return (NULL);
 		}
