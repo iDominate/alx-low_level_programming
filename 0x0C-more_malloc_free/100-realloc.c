7b9dac0e5a8a76265b181b9fa18f14f45ffd27d4
iDominate <ahmedpython2018@gmail.com>
2023-09-07 16:08:10 +0300

    jhju

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+/**
+ * _realloc - similar to realloc
+ *
+ * @ptr: the pointer to array
+ * @old_size: old size of array
+ * @new_size: new size of array
+ *
+ * Return: a new pointer to an array
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	char *char_ptr, *char_ptr2;
+	unsigned int i;
+
+	char_ptr = (char *) ptr;
+	char_ptr2 = (char *) malloc(new_size);
+
+	if (!char_ptr2)
+		return (NULL);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && !ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; (i < new_size) && char_ptr[i]; i++)
+	{
+		char_ptr2[i] = char_ptr[i];
+	}
+	p = (void *) char_ptr2;
+	return (p);
+}
