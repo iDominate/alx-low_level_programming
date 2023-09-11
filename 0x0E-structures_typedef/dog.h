0927b963d753052b22c808c36e797006128c3be8
iDominate <ahmedpython2018@gmail.com>
2023-09-11 14:57:58 +0300

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,15 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 #endif
 
 #ifndef _FUNCTION_H_
 #define _FUNCTION_H_
-
+/**
+ * dog_t - Typedef dog_t
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
