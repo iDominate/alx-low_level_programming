3a302a2cdbe7c211871c5da3f5e4ab95c5723626
iDominate <ahmedpython2018@gmail.com>
2023-09-13 20:31:49 +0300

    new

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * int_index - searches for an int
+ *
+ * @array: array
+ * @size: size of array
+ * @cmp: compare function
+ *
+ * Return: index or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
+
+	result = -1;
+	if (size <= 0)
+		return (result);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) > 0)
+		{
+			result = i;
+			return (result);
+		}
+	}
+	return (result);
+}
