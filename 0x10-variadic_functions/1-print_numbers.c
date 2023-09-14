fa0aed46934485bd59a31ab46062d4e6ccc7a954
iDominate <ahmedpython2018@gmail.com>
2023-09-14 16:08:13 +0300

    new

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,13 @@ void print_numbers(const char *seperator, const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned int i;
-	
+
 	va_start(ptr, n);
 	if (n == 0)
 		return;
 	for (i = 0; i < n; i++)
 	{
-		if(seperator && i != (n - 1))
+		if (seperator && i != (n - 1))
 			printf("%d%s", va_arg(ptr, int), seperator);
 		else
 			printf("%d", va_arg(ptr, int));
