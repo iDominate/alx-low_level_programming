9bdfef5c42460926eda328938e0e9fdd10688e7a
iDominate <ahmedpython2018@gmail.com>
2023-09-14 20:08:54 +0300

    new

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints numbers
+ * print_strings - prints strings
  *
  * @seperator: the sep
  * @n: numbr of args
@@ -13,13 +13,13 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned int i;
-	
+
 	va_start(ptr, n);
 	if (n == 0)
 		return;
 	for (i = 0; i < n; i++)
 	{
-		if(seperator && i != (n - 1))
+		if (seperator && i != (n - 1))
 			printf("%s%s", va_arg(ptr, char *), seperator);
 		else
 			printf("%s", va_arg(ptr, char *));
